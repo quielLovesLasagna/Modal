536ce08ca00df200796a3ba4a459b3b3a0ab00c9
Exequiel Arco <exequielarco23@gmail.com>
2023-10-13 16:26:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 "use strict";
 
+// ---- ELEMENTS ---- //
 const modalBtn = document.querySelector(".modal-btn");
 const modalOverlay = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
+// ---- END OF ELEMENTS ---- //
 
-modalBtn.addEventListener("click", () => {
-  modalOverlay.classList.toggle("open-modal");
-});
+// ---- FUNCTIONS ---- //
+// Open modal
+const openModal = () => modalOverlay.classList.toggle("open-modal");
 
-closeBtn.addEventListener("click", () => {
-  modalOverlay.classList.toggle("open-modal");
-});
+// Clode modal
+const closeModal = () => modalOverlay.classList.toggle("open-modal");
+// ---- END OF FUNCTIONS ---- //
+
+// ---- EVENT LISTENERS ---- //
+// User opens modal
+modalBtn.addEventListener("click", openModal);
+
+// User closes modal
+closeBtn.addEventListener("click", closeModal);
+// ---- END OF EVENT LISTENERS ---- //
